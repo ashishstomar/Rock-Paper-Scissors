6d1e6a874f63be7817690de4a8315fa4ea5ee67b
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-03-18 00:57:41 +0530

    fix loop and score count

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rock-Paper-Scissors
 An implementation of grade-school classic “rock paper scissors.
  Play the game from the browser console.
+
+ To open the console in Chrome, use this keyboard shortcut: "Cmd + Option + J" (on a Mac) or "Ctrl +Shift +J" (on Windows). As an alternative, you can right-click on the webpage and click "Inspect" to open the developer console.
--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-
 // function to randomly return a word from an array
-
 function getComputerChoice() {
   let wordArray = ["rock", "paper", "scissor"];
   ranWord = wordArray[Math.floor(Math.random() * wordArray.length)];
-  console.log("Me: " + ranWord);
   return ranWord;
 }
-
+//function take input to play single round of game
 function playRound(playerSelection, computerSelection) {
   let playerchoice = playerSelection.toUpperCase();
   if ((playerchoice === "ROCK") && (computerSelection === "paper")) {
@@ -27,8 +24,42 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+//function loop to play five rounds with score tracking for both players
+function playGame() {
 
-// playRound(prompt("It's your turn"), getComputerChoice());
+  for ( let i = 0; i <= 4; i++) {
+    const playerSelection = prompt("Your turn. Rock, Paper, Scissor ?");
+    const computerSelection = getComputerChoice();
+    result = (playRound(playerSelection, computerSelection));
+
+    if(result == "It's a TIE!") {
+      console.log(result + " Computer: " + computerSelection + ", You: " + playerSelection + ", Score is " + computerScore + " - " + humanScore);
+    }
+    else if (result == "You Won!") {
+      humanScore++;
+      console.log(result + " Computer: " + computerSelection + ", You: " + playerSelection + ", Score is " + computerScore + " - " + humanScore);
+    }  
+    else {
+      computerScore++;
+      console.log(result + " Computer: " + computerSelection + ", You: " + playerSelection + ", Score is " + computerScore + " - " + humanScore);
+    }
+  }
+}
+
+//initialize score tracking global scope variables
+let humanScore = 0;
+let computerScore = 0;
+
+//invoke function
+playGame()
+
+//conditional statements to decide winner and scores
+if (computerScore > humanScore) {
+  console.log("You Lose! Final Scores -> You: " + humanScore + " Computer: " + computerScore);
+}
+else if (computerScore < humanScore) {
+  console.log("You won! Final Scores -> You: " + humanScore + " Computer: " + computerScore) ;
+}
+else {
+  console.log("It's a TIE! Final Scores -> You: " + humanScore + " Computer: " + computerScore);
+}
